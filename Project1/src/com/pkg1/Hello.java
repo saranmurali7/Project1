4c1e3658c0f18d0f11464c83339baa434a0dda23
saran.muraleedharan@i-exceed.com <saran.muraleedharan@i-exceed.com@IXED-LAP0263>
2019-05-29 14:23:19 +0530

    Commit4

--- Project1/src/com/pkg1/Hello.java
+++ Project1/src/com/pkg1/Hello.java
@@ -4,12 +4,11 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
-		for(int i=0;i<10;i++)
-		{
+		
+		
 			System.out.println("Hello World");
-			System.out.println("Welcome");
-			System.out.println("Arjun");
-		}
+			/*System.out.println("Welcome");
+			System.out.println("Arjun");*/
 	}
 
 }
